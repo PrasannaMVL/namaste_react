b010118007d94f838db3da311cc9d8d60164238b
PrasannaMVL <mandaleeka.prasanna@gmail.com>
2026-02-22 15:14:46 +0530

    error fix

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+const heading = React.createElement("div", { id: "parent" }, 
+  React.createElement("div", { id: "child" }, 
     React.createElement("h1", {}, "I'm h1 tag"),
     React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
+),
 
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child1" }, 
     React.createElement("h1", {}, "I'm h1 tag"),
     React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
+),
+);
 console.log(heading);
-ReactDOM.render(heading, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
